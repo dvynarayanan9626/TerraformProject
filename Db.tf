4ab01c527757b3956359e23e136578899a62a654
dvynarayanan9626 <162408791+dvynarayanan9626@users.noreply.github.com>
2026-03-15 20:26:17 +0530

    V2 : Update Db.tf

--- Db.tf
+++ Db.tf
@@ -5,8 +5,8 @@ resource "aws_db_instance" "default" {
     db_name = "mydatabase"
     identifier = "mydb"
     instance_class = "db.t3.micro"
-    username = "divya"
-    password = "Divya789620"
+    username = "admin"
+    password = "1234567890"
     engine = "MySql"
     engine_version = "8.0"
     deletion_protection = false  
