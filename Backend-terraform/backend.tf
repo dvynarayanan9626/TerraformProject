5af146ed48de7e3f7e324069729891cddd7a4ec3
dvynarayanan9626 <162408791+dvynarayanan9626@users.noreply.github.com>
2026-03-18 20:54:37 +0530

    Update backend.tf

--- Backend-terraform/backend.tf
+++ Backend-terraform/backend.tf
@@ -1,3 +1,9 @@
+# this is used to store the state file into the S3 bucket
+# Note:
+# The S3 bucket should exit before we execute the backend file and if any terraform.tfstate file exits then delte it and run the 
+# backend file or else the terraform state will not be stored in the S3 if already exits(state file before the backend execution).
+
+
 terraform {
   backend "s3" {
     bucket = "divya-terraform-state-file-180326" # when using this backend, the s3 bucket shold created before
