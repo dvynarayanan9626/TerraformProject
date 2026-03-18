fd04a399007a7cfc6d80497d988b1466a166f99b
dvynarayanan9626 <162408791+dvynarayanan9626@users.noreply.github.com>
2026-03-18 20:45:55 +0530

    V2: Update backend.tf

--- Backend-terraform/backend.tf
+++ Backend-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "divya-terraform-state-file-180326"
+    bucket = "divya-terraform-state-file-180326" # when using this backend, the s3 bucket shold created before
     key = "divya/terraform.tfstate"
     region = "us-east-1"    
   }
