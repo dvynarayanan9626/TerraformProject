2626da5c04ee8bca83ff1921b54131b032099c17
dvynarayanan9626 <162408791+dvynarayanan9626@users.noreply.github.com>
2026-03-20 18:41:45 +0530

    Update instance.tf

--- ter/instance.tf
+++ ter/instance.tf
@@ -7,6 +7,9 @@ resource "aws_s3_bucket" "s3_bucket" {
 resource "aws_instance" "Linux" {
   ami           = "ami-0b0b78dcacbab728f"
   instance_type = "t3.micro"
+
+# depends_on is used to s3 bucket will be created firse then ec2 instance will created
+
 depends_on = [aws_s3_bucket.s3_bucket]
   tags = {
     Name = "My_Instance"
