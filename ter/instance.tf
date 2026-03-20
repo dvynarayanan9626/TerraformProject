507d08c529061af0bce7f5d84727f0c57876288e
dvynarayanan9626 <162408791+dvynarayanan9626@users.noreply.github.com>
2026-03-20 18:43:04 +0530

    Update instance.tf
    EC2 +S3

--- ter/instance.tf
+++ ter/instance.tf
@@ -1,9 +1,14 @@
 provider "aws" {
     region = "us-east-2" 
 }
+
+# s3 bucket is creation:
+
 resource "aws_s3_bucket" "s3_bucket" {
     bucket = "divya-terraform-state-file-180326"
 }
+# ec2 instance
+
 resource "aws_instance" "Linux" {
   ami           = "ami-0b0b78dcacbab728f"
   instance_type = "t3.micro"
